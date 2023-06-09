be84475849f887c6a732641230e90438aaeeefd4
Game0ver-gh <69398639+Game0ver-gh@users.noreply.github.com>
2023-06-09 15:12:36 +0200

    l

--- DiseaseSImulation/DiseaseSim.cpp
+++ DiseaseSImulation/DiseaseSim.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
+#include "Window.h"
+
+#include <chrono>
 
 int main(int argc, char* argv[])
 {
-	std::cout << "JD\n";
+	auto* window = Window::GetInstance(L"Test1");
+	auto start = std::chrono::high_resolution_clock::now();
+
+	window->Run([&]() -> bool
+		{
+			auto end = std::chrono::high_resolution_clock::now();
+			if (end - start > std::chrono::seconds(3))
+			{
+				return true;
+			}
+
+			return false;
+		});
 
 	return 0;
 }
